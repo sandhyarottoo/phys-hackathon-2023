aa70407363b0f854aae5fa1ad1e8b3d21c12aa2c
FrancoDB <francodbalso@gmail.com>
2023-11-04 19:32:00 -0400

    Made the player class which is almost ready there is just a drawing issue it seems

--- francosPolar.py
+++ francosPolar.py
@@ -18,9 +18,21 @@ green = (0,255,0)
 blue = (0,0,255)
 white = (255,255,255)
 
+BACKGROUND_COLOR = (255, 255, 255)
+CIRCLE_COLOR = red
+
 #screen width and height
 width = 790
 height = 790
+RING_RADIUS = 300
+PLAYER_RADIUS = 305
+
+PLAYER_VELOCITY = 0.05
+PLAYER_ARC_ANGLE = np.pi / 12  # 90 degrees in radians
+
+player1_angle = -np.pi / 2
+player2_angle = -np.pi / 2
+
 
 #time step for euler integration
 dt = 0.0001
@@ -44,6 +56,34 @@ def a_radial(w,r,v_theta):
 
 def a_tan(w,v_radial,r,ang_acc):
     return w*v_radial+r*ang_acc
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, color, start_angle, keys):
+        super().__init__()
+        self.score = 0
+        self.color = color
+        self.pos = pygame.Vector2(PLAYER_RADIUS, start_angle)
+        self.keys = keys
+        player_width = 8
+        
+        self.image = pygame.Surface((2*PLAYER_RADIUS + player_width , 2*PLAYER_RADIUS + player_width), pygame.SRCALPHA)
+        pygame.draw.arc(self.image, self.color, 
+                        (width/2 - PLAYER_RADIUS, height/2 - PLAYER_RADIUS, (PLAYER_RADIUS + player_width) * 2, (PLAYER_RADIUS + player_width) * 2), 
+                        self.pos.y - PLAYER_ARC_ANGLE / 2, 
+                        self.pos.y + PLAYER_ARC_ANGLE / 2, 
+                        width=player_width)
+        self.rect = self.image.get_rect()
+        #pos_cartesian = polar_to_cartesian(self.pos)
+        self.rect.center = (width/2, height/2)
+    
+    def update(self):
+        keys = pygame.key.get_pressed()
+        # Move the player on the ring
+        if keys[self.keys[0]]:
+            self.pos.y -= PLAYER_VELOCITY  
+        if keys[self.keys[1]]:
+            self.pos.y += PLAYER_VELOCITY   
+
 
 
 class PointCharge(pygame.sprite.Sprite):
@@ -118,10 +158,6 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((790,790))
 pygame.display.set_caption("Circle Sprite")
 
-# Constants
-BACKGROUND_COLOR = (255, 255, 255)
-CIRCLE_COLOR = red
-
 #initial conditions in polar coords
 pos_polar = pygame.Vector2(screen.get_width()/2, np.pi/2)
 vel_polar = pygame.Vector2(-300, 0)
@@ -137,6 +173,16 @@ charge = PointCharge(pygame.Vector2(-100, 0), 100000)
 charges = pygame.sprite.Group()
 charges.add(charge)
 
+# players
+player1_keys = [pygame.K_a, pygame.K_d]
+player2_keys = [pygame.K_LEFT, pygame.K_RIGHT]
+
+player1 = Player("blue", 0, player1_keys)
+player2 = Player("red", np.pi, player2_keys)
+players = pygame.sprite.Group()
+players.add(player1)
+players.add(player2)
+
 # Main game loop
 running = True
 
@@ -150,13 +196,16 @@ while running:
 
     # Clear the screen
     screen.fill(BACKGROUND_COLOR)
+    pygame.draw.circle(screen, (160,160,160), (width / 2, height / 2), RING_RADIUS)
 
     circles.update(charges)
-    charges.draw(screen)
     circles.draw(screen)
-    
-    
 
+    charges.draw(screen)
+    
+    players.update()
+    players.draw(screen)
+    
    # Update the display
     pygame.display.flip()
     dt = clock.tick(60) / 1000
